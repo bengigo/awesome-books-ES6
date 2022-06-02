72d97e2c9cc7be778215c1a09a67ad0f0a135dc0
bengigenc <genc.bengi@gmail.com>
2022-06-02 13:55:56 +0300

    Create a function to display only form page

--- modules/listPage.js
+++ modules/listPage.js
@@ -1,4 +1,4 @@
-function displayList() {
+export default function displayList() {
   const list = document.querySelector('#list');
   const addNew = document.querySelector('#add-new');
   const contact = document.querySelector('#contact');
@@ -7,8 +7,8 @@ function displayList() {
   const contactPage = document.querySelector('contact-page');
 
   addNew.addEventListener('click', () => {
-    formPage.style.display = 'flex';
-    listPage.style.display = 'none';
+    formPage.style.display = 'none';
+    listPage.style.display = 'flex';
     contactPage.style.display = 'none';
   });
 }
