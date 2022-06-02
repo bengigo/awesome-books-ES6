fe9d7cbbd839894729385ea75c0d8ae4192f4172
bengigenc <genc.bengi@gmail.com>
2022-06-02 15:13:54 +0300

    display list page with js and css

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
+// WHERE WILL TIME STUFF GO?
 
+
+// import book methods
+import Book from './modules/bookMethods.js';
+
+// import adding event
+import addBook from './modules/addBook.js';
+
+// import list page
+import displayList from './modules/listPage.js';
+
+// import form page
+import displayForm from './modules/formPage.js';
+
+// import contact page
+import displayContact from './modules/contactPage.js'
+
+
+const bookTitle = document.querySelector('#title');
+const authorName = document.querySelector('#author');
+const addButton = document.querySelector('#add-button');
 
 // display book (construct book in here)
+Book.displayBooks();
 
 // delete book
+Book.deleteBooks();
 
 // store books
+Book.storeBooks();
 
 // add book
-
-// call display book
-
-// call delete book
-
+addBook();
 
 // (page management) navigation
+
+displayList();
+
+displayForm();
+
+displayContact();
+
+
--- modules/listPage.js
+++ modules/listPage.js
@@ -1,14 +1,17 @@
 export default function displayList() {
   const list = document.querySelector('#list');
-  const addNew = document.querySelector('#add-new');
-  const contact = document.querySelector('#contact');
+  // const addNew = document.querySelector('#add-new');
+  // const contact = document.querySelector('#contact');
   const listPage = document.querySelector('#display-section');
-  const formPage = document.querySelector('add-page');
-  const contactPage = document.querySelector('contact-page');
+  const formPage = document.querySelector('#add-page');
+  const contactPage = document.querySelector('#contact-page');
 
-  addNew.addEventListener('click', () => {
+  list.addEventListener('click', () => {
     formPage.style.display = 'none';
+    // formPage.classList.toggle('hide');
     listPage.style.display = 'flex';
+    // listPage.classList.toggle('hide');
     contactPage.style.display = 'none';
+    // contactPage.classList.toggle('hide');
   });
 }
